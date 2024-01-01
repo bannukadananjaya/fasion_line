2a7c4c842b65e9449e026d03055f754ba0e1ad8e
Subasinghe S.A.B.D <banukasubasinghe13@gmail.com>
2024-01-01 11:36:58 +0530

    add basic files for web app

---
+++ src/components/toolbox/Topbar.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import "./topbar.css"
+
+
+function Topbar() {
+  return (
+    <div className="topbarContainer">
+      <div className="topbarLeft">
+        <span className="logo"></span>
+      </div>
+      <div className="topbarright"></div>
+      <div className="topbarcenter"></div>
+
+    </div>
+  )
+}
+
+
+
+export default Topbar
